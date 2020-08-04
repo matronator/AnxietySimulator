900e0d6b05864caab7faa66c7041fef4d16afb4c
Matronator <info@matronator.com>
2020-08-04 11:19:21 +0200

    finally

--- src/js/components/ProgressBar/ProgressBar.js
+++ src/js/components/ProgressBar/ProgressBar.js
@@ -1,21 +1,52 @@
-class ProgressBar extends HTMLElement {
-    static get observedAttributes() {
-        return ['min', 'max', 'value', 'fill']
-    }
-    async connectedCallback() {
-        const res = await fetch(`src/js/components/ProgressBar/ProgressBar.html`)
+fetch(`src/js/components/ProgressBar/ProgressBar.html`)
+    .then(stream => stream.text())
+    .then(text => define(text))
 
-        this.shadow = this.attachShadow({ mode: `open` })
-        this.shadow.innerHTML = await res.text()
+function define(html) {
+    class CustomProgress extends HTMLElement {
+        static get observedAttributes() {
+            return ['max', 'value', 'fill']
+        }
+        set value(value) {
+            this._value = parseFloat(value)
+            this._fill = parseFloat(value) / this._max * 100
+        }
+        get value() {
+            return parseFloat(this._value)
+        }
+        set max(max) {
+            this._max = max
+        }
+        get max() {
+            return parseFloat(this._max)
+        }
+        set fill(fill) {
+            this._fill = fill
+            const bar = this.shadow.querySelector(`.progress-bar-fill`)
+            if (bar) {
+                bar.setAttribute(`style`, `width: ${this._fill}%`)
+            }
+        }
+        get fill() {
+            return parseFloat(this._fill)
+        }
+        constructor() {
+            super()
+            const shadow = this.attachShadow({ mode: `open` })
+            shadow.innerHTML = html
+            this.shadow = shadow
+            this.max = 100
+            this.fill = 50
+        }
+        attributeChangedCallback(name, oldValue, newValue) {
+            if (name === `value` && oldValue !== newValue) {
+                this.value = parseFloat(newValue)
+                const bar = this.shadow.querySelector(`.progress-bar-fill`)
+                if (bar) {
+                    bar.setAttribute(`style`, `width: ${this.fill}%`)
+                }
+            }
+        }
     }
-    updateFill(num) {
-        let barFill = parseFloat(this.getAttribute(`fill`))
-        barFill += num
-        const newBarFill = Number(((barFill) / (Number(this.getAttribute(`max`)))) * 100).toFixed(3)
-        this.setAttribute(`value`, barFill)
-        this.setAttribute(`fill`, barFill)
-        this.progressBar.dataset.barFill = newBarFill
-        this.progressBarFill.setAttribute(`style`, `width: ${barFill}%;`)
-    }
+    customElements.define(`progress-bar`, CustomProgress)
 }
-customElements.define(`progress-bar`, ProgressBar)
