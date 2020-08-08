b91512c07bc3f1a21b374f0c3c11ab9ce1fc9db6
Matronator <info@matronator.com>
2020-08-08 18:11:33 +0200

    yay

--- src/js/components/FocusBar.js
+++ src/js/components/FocusBar.js
@@ -1,60 +1,60 @@
 class FocusBar {
-    constructor(scene, x, y, width, height) {
+    constructor(scene, x, y, width, height, image) {
         this.bar = new Phaser.GameObjects.Graphics(scene)
 
         this.x = x
         this.y = y
         this.width = width
         this.height = height
-        this.value = 50
+        this.value = 0
         this.p = (this.width - 4) / 100
 
         this.draw()
 
         scene.add.existing(this.bar)
+        scene.add.image(this.width / 2, this.y + (this.height / 2) + 5, 'bar')
     }
 
     less(amount) {
         this.value -= amount
 
-        if (this.value < 0) {
-            this.value = 0
+        if (this.value < -50) {
+            this.value = -50
         }
 
         this.draw()
 
-        return (this.value === 0)
+        return (this.value === -50)
     }
 
     more(amount) {
-        if (amount < 0) {
-            return this.less(Math.abs(amount))
-        }
         this.value += amount
 
-        if (this.value > 100) {
-            this.value = 0
+        if (this.value > 50) {
+            this.value = 50
         }
 
         this.draw()
 
-        return (this.value === 0)
+        return (this.value === 50)
     }
 
     draw() {
         this.bar.clear()
 
         //  BG
-        this.bar.fillStyle(0xffffff)
-        this.bar.fillRect(this.x, this.y, this.width, this.height)
-
-        const hue = (360 - (Math.abs(50 - this.value) - 68)) / 360
-
+        const hue = (360 - (Math.abs(this.value) - 68)) / 360
         this.bar.fillStyle(Phaser.Display.Color.HSLToColor(hue, 1, 0.5).color)
 
-        const d = Math.floor(this.p * this.value)
+        this.bar.fillRect(this.x, this.y, this.width, this.height)
 
-        this.bar.fillRect(this.x + 2, this.y + 2, d, this.height - 4)
+        this.bar.fillStyle(0xffffff)
+
+        const d = Math.floor(this.p * (this.value + 50))
+        this.bar.lineStyle(8, 0xffffff)
+        this.bar.lineBetween(d, this.y, d, this.height + 10)
+
+        // this.bar.fillRect(this.x + 2, this.y + 2, d, this.height - 4)
     }
 }
 
--- src/js/index.js
+++ src/js/index.js
@@ -3,8 +3,8 @@ import FocusBar from './components/FocusBar'
 
 const config = {
     type: Phaser.AUTO,
-    width: 800,
-    height: 600,
+    width: window.innerWidth,
+    height: window.innerHeight,
     scene: {
         preload,
         create,
@@ -14,15 +14,57 @@ const config = {
 
 const game = new Phaser.Game(config)
 let fb
-let momentum = 0.1
+let acceleration = 0
+let velocity = 0
+let drag = 0.87
+let focus = 0
+
+let keyQ
+let keyE
 
 function preload() {
+    this.load.image('bar', 'src/images/bar.png')
 }
 
 function create() {
-    fb = new FocusBar(this, 0, 100, game.scale.width, 20)
+    fb = new FocusBar(this, 0, 100, game.scale.width, 58, 'bar')
+    fb.value = focus
+    keyQ = this.input.keyboard.addKey(Phaser.Input.Keyboard.KeyCodes.Q)
+    keyE = this.input.keyboard.addKey(Phaser.Input.Keyboard.KeyCodes.E)
 }
 
 function update() {
-    fb.more(momentum)
+    if (keyQ.isDown) {
+        acceleration -= 0.02
+    } else if (keyE.isDown) {
+        acceleration += 0.02
+    }
+    updateFocus()
+}
+
+let momentum = 0
+
+function updateFocus() {
+    if (acceleration < -0.5) {
+        acceleration = -0.5
+    } else if (acceleration > 0.4) {
+        acceleration = 0.4
+    }
+    velocity += acceleration
+    velocity *= drag
+    if (velocity < -2) {
+        velocity = -2
+    } else if (velocity > 2) {
+        velocity = 2
+    }
+    if (focus < 0) {
+        momentum = velocity + (-0.5 + Math.abs(focus / 50))
+    } else {
+        momentum = velocity + (0.5 - (focus / 50))
+    }
+    if (velocity >= 0) {
+        focus = fb.more(Math.abs(velocity))
+    } else {
+        focus = fb.less(Math.abs(velocity))
+    }
 }
